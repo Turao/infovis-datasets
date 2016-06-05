20a341eeed713ace95c3531fbafe5db1057eae08
Luis Henrique Grassi <lhmgrassi@gmail.com>
2016-06-05 02:53:51 +0200

    no message

--- chart4.js
+++ chart4.js
@@ -5,123 +5,159 @@ Object.values = function(o){return Object.keys(o).map(function(k){return o[k]})}
 d3.tsv("https://raw.githubusercontent.com/Turao/infovis-datasets/master/cancerData.tsv",
 	function(err, d) {
 
-		// binding data
-		var chartData = d;
-
-		var total = 0;
-		chartData.forEach(function(cancerType) {
-			total += parseFloat(cancerType['Children'])
-							+ parseFloat(cancerType['Mid-Adults'])
-							+ parseFloat(cancerType['Older Adults']);
-    });
-		console.log(total);
-
-		var percentages = [];
+	// binding data
+	var chartData = d;
+	var percentages = [];
 
     // % of distribution independent of age
     chartData.forEach(function(cancerType) {
     	var dict = {};
     	dict['Site'] = cancerType['Site'];
-    	dict['Percentage'] = (
-    											parseFloat(cancerType['Children'])
-									    	+ parseFloat(cancerType['Mid-Adults'])
-									    	+ parseFloat(cancerType['Older Adults'])
-									    	) / total;
-			percentages.push(dict);
+    	dict['Percentage'] = parseFloat(cancerType['PChildren'])/100;
+    	percentages.push(dict);
+
+    	var dict = {};
+    	dict['Site'] = cancerType['Site'];
+    	dict['Percentage'] = parseFloat(cancerType['PMid-Adults']/100);
+    	percentages.push(dict);
+    	
+    	var dict = {};
+    	dict['Site'] = cancerType['Site'];
+    	dict['Percentage'] = parseFloat(cancerType['POlder Adults'])/100;									    	    	    			
+		percentages.push(dict);
     });
-		console.log('percentages:', percentages);
+		
 
+	// ****************
+	// MAKING THE CHART
+	// ****************
 
+	// chart's properties
+	var margin = {top: 50, right: 20, bottom: 100, left: 100},
+			height = 240,
+			width = 420;
 
-		// ****************
-		// MAKING THE CHART
-		// ****************
+	// bars' properties
+	var barWidth = 10;
 
-		// chart's properties
-		var margin = {top: 50, right: 20, bottom: 100, left: 100},
-				height = 240,
-				width = 420;
-
-		// bars' properties
-		var barWidth = 20;
-
-		// adds the bars svg to the chart's div
-		var chart = d3.select('#ch4').append("svg")
-		  .attr('width', width + margin.left + margin.right)
-		  .attr('height', height + margin.top + margin.bottom)
-		  .append("g")
-    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	// adds the bars svg to the chart's div
+	var chart = d3.select('#ch4').append("svg")
+	  .attr('width', width + margin.left + margin.right)
+	  .attr('height', height + margin.top + margin.bottom)
+	  .append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     chart.append("text")
         .attr("x", (width / 2))             
         .attr("y", 0 - (margin.top / 2))
         .attr("text-anchor", "middle")  
         .style("font-size", "14px") 
-        .text("Distribution of cancer independent of age");
+        .text("Distribution of cancer by group");
 
 
-
-    // scales
-		var y = d3.scale.linear()
-				.domain([0, 1])
-		    .range([height, 0]);
-
-		var x = d3.scale.ordinal()
-		    .domain(percentages.map(function (percentage) {
-		    												return percentage['Site'];
-		    											})
-		    )
-		    
-		    .rangeRoundBands([0, width])
-		var color = d3.scale.category10();
-
-
-
-		// bars'
-		chart.selectAll('rect').data(percentages)
-			.enter().append('rect')
-			.style({'stroke': 'black', 'stroke-width': '0.2'})
-			.attr('fill', function (data, i) {
-				return color(i);
+	var tip = d3.tip()
+			.attr('class', 'd3-tip')
+			.offset([-10, 0])
+			.html(function(d) {
+			return "<span style='color:white'>" + (d['Percentage'] * 100).toFixed(1) + "</span>";
 			})
-			.attr('width', barWidth)
-			.attr('height', function (data) {
-					return height - y(data['Percentage']);
-			 	})
-			.attr('x', function (data, i) {
-			 		return x(data['Site'])
-			 		// + offset to center the bar with the column name
-			 		 + width/(Object.keys(percentages).length*2)
-			 		 - barWidth/2;
-			 	})
-			.attr('y', function (data) {
-			  	return y(data['Percentage']);
-	 		});
+
+    var color_hash = {  
+    					0 : ["Child", "#009292"],
+					    1 : ["Mid-Adult", "#B66DFF"],
+					    2 : ["Old Adult", "#DC6D00"]
+					 }
+
+	// scales
+	var y = d3.scale.linear()
+			.domain([0, 0.5])
+	    .range([height, 0]);
+
+	var x = d3.scale.ordinal()
+	    .domain(percentages.map(function (percentage) {
+	    												return percentage['Site'];
+	    											})
+	    )
+	    .rangeRoundBands([0, width])
+	
+	var color = d3.scale.category10();
 
 
+	// bars'
+	chart.selectAll('rect').data(percentages)
+		.enter().append('rect')
+		.style({'stroke': 'black', 'stroke-width': '0.2'})
+		.attr('fill', function (data, i) {
+		 		return color_hash[String(i % 3)][1];
+		 	})
+		.attr('width', barWidth)
+		.attr('height', function (data) {				
+				return height - y(data['Percentage']);
+		 	})
+		.attr('x', function (data, i) {
+		 		return x(data['Site']) + (i % 3) * barWidth + (barWidth * 1.5);
+		 	})
+		.attr('y', function (data) {
+		  	return y(data['Percentage']);
+ 		})
+		.on('mouseover', tip.show)
+  		.on('mouseout', tip.hide);
+
+	chart.call(tip);
+
+	//x-axis
+	var xAxis = d3.svg.axis()
+	  .scale(x)
+	  .orient("bottom");
+	    
+	chart.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+	  
+	//y-axis
+	var yAxis = d3.svg.axis()
+	  .scale(y)
+	  .orient("left")
+	  .tickFormat(d3.format("%"));
+	  
+	chart.append("g")
+	  .attr("class", "y axis")
+	  .call(yAxis);
+
+	// add legend   
+	var legend = chart.append("g")
+	  .attr("class", "legend")
+	  .attr("x", width - 65)
+	  .attr("y", 25)
+	  .attr("height", 100)
+	  .attr("width", 100);
+
+	legend.selectAll('g').data(percentages)
+      .enter()
+      .append('g')
+      .each(function(d, i) {
+        var g = d3.select(this);
+      
+        g.append("rect")
+          .attr("x", 50)
+          .attr("y", (i % 3 ) * 25)
+          .attr("width", 10)
+          .attr("height", 10)
+          .style("fill", color_hash[String(i % 3)][1]);
+        
+        g.append("text")
+          .attr("x", 65)
+          .attr("y", (i % 3) * 25 + 10)
+          .attr("height",30)
+          .attr("width",100)
+          .style("fill", color_hash[String(i % 3)][1])
+          .text(color_hash[String(i % 3)][0]);
+      });
+
+});
 
 
-			//x-axis
-			var xAxis = d3.svg.axis()
-			  .scale(x)
-			  .orient("bottom");
-			    
-			chart.append("g")
-			  .attr("class", "x axis")
-			  .attr("transform", "translate(0," + height + ")")
-				.call(xAxis);
-
-			  
-			//y-axis
-			var yAxis = d3.svg.axis()
-			  .scale(y)
-			  .orient("left")
-			  .tickFormat(d3.format("%"));
-			  
-			chart.append("g")
-			  .attr("class", "y axis")
-			  .call(yAxis);
-
-	});
 
  
